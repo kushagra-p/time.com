b6e0e1c2ee62a676dc834514ecbc564018ce2fca
Kushagra <kushagraprasad05@gmail.com>
2021-11-20 14:23:12 +0530

    Added functionality to scrape latest stories

---
+++ api/routes/latest-router.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = function(app) {
+  var todoList = require('../service/latest-service');
+
+  // latest-stories Routes
+  app.route('/getLatestStories')
+    .get(todoList.get_latest_stories);
+
+};
+
+
---
+++ api/service/latest-service.js
@@ -0,0 +1,65 @@
+
+var jsonObj
+
+var jsdom = require("jsdom");
+var JSDOM = jsdom.JSDOM;
+
+exports.get_latest_stories = function(req,res) {
+    const https = require('https')
+const url = "https://time.com";
+const result=https.get(url, val => {
+  let data = '';
+  val.on('data', chunk => {
+    data += chunk;
+  });
+  val.on('end', () => {
+const rs=convertHtml(data.toString(),'ol')
+const ret=prepareRes(rs)
+res.send(ret)
+  })
+}).on('error', err => {
+  console.log(err.message);
+})
+}
+function prepareRes(rs){
+  let res=[]
+  rs.children.forEach(data=>{
+     if( data['children'][0]){
+      data['children'][0]['children'][0]['children'].forEach(ele=>{
+          if(ele['tagName']=='H2')
+          {res.push(ele['children'][0])}
+      })
+      }
+  })
+  let ret=[]
+  res.forEach(data=>{
+      let obj={
+          title:data['textContent'],
+          link:"https://time.com"+data['attributes'][0][1]
+      }
+      ret.push(obj)
+  })
+  jsonObj=ret
+  return ret
+}
+function convertHtml(html,selector)
+{
+    let document = new JSDOM(html.toString()).window.document;
+const Elem = e => ({
+    toJSON : () => ({
+      tagName: 
+        e.tagName,
+      textContent:
+        e.textContent,
+      attributes:
+        Array.from(e.attributes, ({name, value}) => [name, value]),
+      children:
+        Array.from(e.children, Elem)
+    })
+  })
+  
+  const html2json = e =>
+    JSON.stringify(Elem(e), null, '  ')
+    return (JSON.parse(html2json(document.querySelector(selector))))
+
+}
